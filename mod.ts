a3c332920e95be16ec55d86c0b079e358bb96e3a
Anna <69672938+AnnaCodes@users.noreply.github.com>
2021-01-14 16:01:47 +0530

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -7,6 +7,7 @@ let envperm = false;
 try { Deno.env.get("NOTHING"); envperm = true; } catch(e) { envperm = false; }
 
 let token = envperm && Deno.env.get("BOT_TOKEN") ? Deno.env.get("BOT_TOKEN") : config.token;
+log("Debug", "Using token: " + token);
 
 log("Bot", "Connecting...");
 const client = new MusicBoxBot();
